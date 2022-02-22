6c6bdcb6025ea5c542ec50e03fe04da7d8568843
Julio <jcmcambar@gmail.com>
2022-02-22 20:16:37 +0100

    Cambiando a https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=oLFJ5B4xCF03jCN5nszF6WDSmnaZE1ZO`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=oLFJ5B4xCF03jCN5nszF6WDSmnaZE1ZO`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(gif => {
